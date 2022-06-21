21c56867a1eb50ec78572437d20d647ffe086fd5
Renato de Souza <renbstux@gmail.com>
2022-06-21 13:20:25 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/renbstux/payroll/services/PaymentService.java
@@ -0,0 +1,14 @@
+package com.renbstux.payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.renbstux.payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+	
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob", 200.0, days);
+	}
+
+}
